49c5a3ad462b4605bbbd388920c357e5a8ea649a
lalit.nailwal@ebix.com <lalit.nailwal@ebix.com>
2020-08-02 23:17:02 +0530

    Added employee photo 52 - 53

--- EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
+++ EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -1,18 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EmployeeManagement.Models
+namespace EmployeeManagement.ViewModels
 {
-    public class Employee
-    {
-        public int Id { get; set; }
+    public class EmployeeCreateViewModel
+    {      
         [Required, MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
-        [Display(Name = "Office Email")]
+       
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
             ErrorMessage = "Invalid email format")]
         [Required]
@@ -20,7 +20,6 @@ namespace EmployeeManagement.Models
 
         [Required]
         public Dept? Department { get; set; }
-        public string PhotoPath { get; set; }
+        public IFormFile Photo { get; set; }
     }
-
 }
